a37d23dc761a8a3587d02ca3e15877ef9634935b
ihabChaker <hi_benakcha@esi.dz>
2025-11-08 13:05:59 +0100

    added apiOmk.html

---
+++ modules/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail:'samuel.szoniecky@univ-paris8.fr',
+    apiOmk:'http://localhost/omk_thyp_25-26/api/',
+    ident: 'kdJxd8ZzQ8vGOxIUBCrpqoARa5vASKOi',
+    key:'7eijgG7BlJig7vWdS0ba1Tj4TZF5c12B',
+    keyGitHub:''
+}
